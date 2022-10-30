744a117d440ed2f132403c98ee76d737d8395d5e
JJTTs <jungletsukiyo@gmail.com>
2022-10-30 23:32:31 +0900

    バース完了

--- js/test.js
+++ js/test.js
@@ -217,6 +217,7 @@
         });
 
     // 6.データを継承（ローカルストレージの内容を取り込む）
+    // ブラウザを更新しても、継承後、続きから再開できる。ただし、記録していることが前提
     $('#ochiai_inherit').on('click',function(){
         Object.keys(ochiai_result).forEach(function(key){
         ochiai_result[key]=localStorage.getItem('ochiai_result_'+key);
@@ -231,5 +232,192 @@
     //     for(let i =0; i<100; i++){
     //     setTimeout(ochiai_simulation(),1000);}
     // });
+
+
+// バース対応
+// 落合をコピー後、Ctrl+hで置換
+
+    let bass_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0};
+
+
+    //  左から、累計で［シングルヒット、2ベースヒット、3ベースヒット、ホームラン、三振、四球・死球、アウト（ゴロ、フライ）］
+    let bass_record=[96,127,129,176,246,330,541];
+    console.log(bass_record);
+
+    //  乱数は100で統一するので、上記結果を標準化
+    let bass_record_standard=[];
+    bass_record.forEach(function(value,index){
+        bass_record_standard[index]=bass_record[index]/(bass_record[6]/100);
+    });
+    console.log(bass_record_standard);
+
+    // 関数の呼び出しのテスト
+    // function bass_console(msg){
+    //     console.log(msg);
+    // }
+    // $('#bass_nine').on('click',function(){
+    //     bass_console(bass_record);
+    // });
+
+
+
+   // 関数の切り出し
+    function bass_simulation(){
+
+    bass_result.daseki ++;
+    let daseki =0;
+    daseki++
+
+    // // アウトが３未満なら、交代という表示を出さない
+    // if(out_num<3){
+    //     $("#stop-game").text("");
+    // }
+    
+    let probability = Math.random() * 100;
+    let record_view =bass_record_standard;
+    console.log(probability);
+    // console.log((single+double+triple+homerun)/(daseki-fourball));
+    
+    // 100までの乱数が結果配列のどの範囲に含まれるかで、結果を分別
+    if(probability<record_view[0]){            
+        $("#result").text("シングルヒット");
+        runner ++;
+        bass_result.single ++;
+        console.log("ヒット");
+        if(runner>3){
+            runner = 3;
+            score ++;
+        }
+        
+    }else if(probability<record_view[1]){
+        $("#result").text("2塁打");
+        bass_result.double ++;
+        console.log("2塁打");
+        if(runner>=2){
+            runner = 2;
+            score = score + runner -1;
+        }else{
+            runner ++;
+        }
+    }else if(probability<record_view[2]){
+        $("#result").text("3塁打");
+        bass_result.triple ++;
+        console.log("3塁打");
+        score +=runner;
+        runner=1;
+
+    }else if(probability<record_view[3]){
+        $("#result").text("ホームラン");
+        score ++;
+        bass_result.homerun ++;
+        score +=runner;
+        runner=0;
+        console.log("ホームラン");
+    }else if(probability<record_view[4]){
+        $("#result").text("三振");
+        out_num++;
+        out_num_total++;
+        bass_result.strikeout ++;
+        console.log("三振");
+    }else if(probability<record_view[5]){            
+        $("#result").text("四球・死球");
+        runner ++;
+        bass_result.fourball ++;
+        console.log("四球・死球");
+        if(runner>3){
+            runner = 3;
+            score ++;
+        }
+    }else if(probability<record_view[6]){
+        $("#result").text("アウト（ゴロ、フライ）");
+        out_num++;
+        out_num_total++;
+        bass_result.mishit ++;
+        console.log("アウト（ゴロ、フライ）");
+    }
+    //  // カウンタを動かす
+    // if(out_num===3){
+    //     $("#stop-game").text("3アウト交代");
+    // }
+
+    console.log(out_num,'out');
+    console.log(score,'score');
+    console.log(runner,'runner');
+    let averagecount = Math.floor(score/(inning /9)*100)/100;
+    bass_result.average_score=averagecount;
+    let hit_rate = Math.floor(
+        (bass_result.single+bass_result.double+bass_result.triple+bass_result.homerun)/(bass_result.daseki-bass_result.fourball)*1000
+        );
+    bass_result.hit_rate =hit_rate;
+     // カウンタを表示
+    $("#status_bass").text('バース1986：'+inning+'回、'+out_num+'アウト、'+runner+'人が出塁中、'+'ここまで'+score+'点をとられました。1試合平均では'+averagecount+'点、とられています');
+    $("#total_bass").text('バース1986：全'+bass_result.daseki+'打席のうち、1塁打'+bass_result.single+'回、2塁打'+bass_result.double+'回、3塁打'+bass_result.triple+'回、ホームラン'+bass_result.homerun+'回、三振'+bass_result.strikeout+'回、四球'+bass_result.fourball+'回、凡打'+bass_result.mishit+'回、打率は.'+hit_rate+'です。');
+    if(out_num<3){
+    return{
+        out_num,
+        score,
+        runner,
+    }
+    }else{
+    out_num=0;
+    runner=0;
+    inning++;
+    return{
+        out_num,
+        score,
+        runner,
+        inning
+        }
+        }
+
+    }
+
+    // 1.1打席毎計算
+    // clickのあとに直接、関数名を入れても動かなくてエラーが出る。
+    $('#bass').on('click',function(){
+        bass_simulation();
+    });
+
+    // 2.9イニング毎計算。whileでやるしかない。
+    $('#bass_nine').on('click',function(){
+        out_num_total=0;
+        while(out_num_total<27){
+        bass_simulation();
+        }
+    });
+
+    // 3.100打席毎計算
+    $('#bass_hundred').on('click',function(){
+        for(let i =0; i<100; i++){
+            
+        bass_simulation();
+        }
+    });
+
+    // 4.総データを記録
+    $('#bass_add').on('click',function(){
+        Object.keys(bass_result).forEach(function(key){
+        localStorage.setItem('bass_result_'+key,bass_result[key]);
+        });
+        alert("successfully saved");
+        });
+
+    // 5.総データを破棄
+    $('#bass_delete').on('click',function(){
+        Object.keys(bass_result).forEach(function(key){
+        localStorage.removeItem('bass_result_'+key);
+        });
+        alert("successfully deleted");
+        });
+
+    // 6.データを継承（ローカルストレージの内容を取り込む）
+    // ブラウザを更新しても、継承後、続きから再開できる。ただし、記録していることが前提
+    $('#bass_inherit').on('click',function(){
+        Object.keys(bass_result).forEach(function(key){
+        bass_result[key]=localStorage.getItem('bass_result_'+key);
+        });
+        console.log(bass_result);
+        alert("successfully inherited");
+        });
 
 
