85fb63b6539f19f548c8e53dd04d5117f238671b
JJTTs <jungletsukiyo@gmail.com>
2022-11-03 13:02:55 +0900

    変数の切り分け

--- js/test.js
+++ js/test.js
@@ -27,11 +27,13 @@
 // ４．9回一括、100打席一括の繰り返し処理
 
 //    各種カウントの設定
+// 実際のゲームだと、同じアウトカウントを共有するので、グローバル変数が妥当。ただ、今回の想定は、すべての打席を同じ選手が立つ場合なので、グローバル変数だと、
+// 落合が1打席打ってアウトになると、バースは1アウトの状態で始まる。100打席とか一気に計算するうえでは誤差だけど、選手データにもたせることで精緻化
+// もはや不要になったものの、どういうことになるのか、コンソールで確認できるように残しておく
     let out_num = 0;
     let out_num_total = 0;
-    let score = 0;
     let runner = 0;
-    let inning=1;
+    
 
 // 複数選手の累計データを保有するには、以下の変数だと不便になったので、オブジェクトを利用。
 // let daseki =0;
@@ -45,10 +47,14 @@
 // let hit_rate = (single+double+triple+homerun)/(daseki-fourball);
 
 // 選手打席データ
-    let ochiai_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0};
-    let bass_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0};
-    let matsunaka_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0};
-    let murakami_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0};
+// 当初、イニング数、スコアは前回からの流れで全体共通の変数としていた。ただ、そうすると、9回を一気に進める機能を使った際、
+// 落合が９０回に進んだあとで、バースの9回進行ボタンを押すと、バースの結果が99回になってしまう。点数も引き継がれたような結果となった。
+// そのため、イニング数、スコアも、各自のデータとして保有するように変更。
+// アウト数も、よくよく見てみると、おかしな動き。選手データに入れることにする。
+    let ochiai_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0,innings:1,scores:0,out_num:0,out_num_total:0,runner:0};
+    let bass_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0,innings:1,scores:0,out_num:0,out_num_total:0,runner:0};
+    let matsunaka_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0,innings:1,scores:0,out_num:0,out_num_total:0,runner:0};
+    let murakami_result = {daseki:0,single:0,double:0,triple:0,homerun:0,fourball:0,strikeout:0,mishit:0,hit_rate:0,average_score:0,innings:1,scores:0,out_num:0,out_num_total:0,runner:0};
 
 //  左から、累計で［シングルヒット、2ベースヒット、3ベースヒット、ホームラン、三振、四球・死球、アウト（ゴロ、フライ）］
     let ochiai_record=[92,116,117,169,209,313,568];
@@ -77,12 +83,22 @@
         murakami_record_standard[index]=murakami_record[index]/(murakami_record[6]/100);
     });
 
+
+// 選手毎の回数、総得点カウンタ
+    let ochiai_inning;
+    let bass_inning;
+    let matsunaka_inning;
+    let murakami_inning;
+
+    let ochiai_score=0;
+    let bass_score=0;
+    let matsunaka_score=0;
+    let murakami_score=0;
+
 // 関数の切り出し
     function simulation(result, record_standard, status_id, total_id, name_year){
 
     result.daseki ++;
-    let daseki =0;
-    daseki++
 
     let probability = Math.random() * 100;
     let record_view =record_standard;
@@ -94,37 +110,42 @@
         runner ++;
         result.single ++;
         console.log("ヒット");
+        // ランナーを加算した結果、3人より多くなれば、1点入って、ランナーは3人にする。
         if(runner>3){
             runner = 3;
-            score ++;
+            result.scores ++;
         }        
     }else if(probability<record_view[1]){
         $("#result").text("2塁打");
         result.double ++;
         console.log("2塁打");
+        // ランナーが2人以上いれば、ホームに帰ってくるので、点数に加算。合わせて、残っているランナーを調整。
         if(runner>=2){
             runner = 2;
-            score = score + runner -1;
+            result.scores = result.scores + runner -1;
         }else{
             runner ++;
         }
     }else if(probability<record_view[2]){
         $("#result").text("3塁打");
         result.triple ++;
+        // すでにいるランナーはホームに帰ってくるので、点数に加算
         console.log("3塁打");
-        score +=runner;
+        result.scores +=runner;
         runner=1;
     }else if(probability<record_view[3]){
         $("#result").text("ホームラン");
-        score ++;
+        result.scores ++;
         result.homerun ++;
-        score +=runner;
+        result.scores +=runner;
         runner=0;
         console.log("ホームラン");
     }else if(probability<record_view[4]){
         $("#result").text("三振");
         out_num++;
         out_num_total++;
+        result.out_num++;
+        result.out_num_total++;
         result.strikeout ++;
         console.log("三振");
     }else if(probability<record_view[5]){            
@@ -134,43 +155,56 @@
         console.log("四球・死球");
         if(runner>3){
             runner = 3;
-            score ++;
+            result.scores ++;
         }
     }else if(probability<record_view[6]){
         $("#result").text("アウト（ゴロ、フライ）");
         out_num++;
         out_num_total++;
+        result.out_num++;
+        result.out_num_total++;
         result.mishit ++;
         console.log("アウト（ゴロ、フライ）");
     }
 
     console.log(out_num,'out');
-    console.log(score,'score');
+    console.log(out_num_total,'out_total');
+    console.log(result.out_num,'result.out');
+    console.log(result.out_num_total,'result.out_total');
+    console.log(result.scores,'score');
     console.log(runner,'runner');
-    let averagecount = Math.floor(score/(inning /9)*100)/100;
-    result.average_score=averagecount;
+    console.log(result.runner,'result.runner');
+
+    // 1試合（9回）平均の得点数。小数点を出すために、100倍してから端数を落として、その後100で割る
+    let average_count = Math.floor(result.scores/(result.innings /9)*100)/100;
+    result.average_score=average_count;
+    // 打率の計算
+    // ローカルストレージから呼び出すと、数字がおかしくなる。記録されているデータは正しく見える。
     let hit_rate = Math.floor(
         (result.single+result.double+result.triple+result.homerun)/(result.daseki-result.fourball)*1000
         );
     result.hit_rate =hit_rate;     
-    $(status_id).text(name_year+'：'+inning+'回、'+out_num+'アウト、'+runner+'人が出塁中、'+'ここまで'+score+'点をとられました。1試合平均では'+averagecount+'点、とりました');
+    // 結果の表示
+    $(status_id).text(name_year+'：'+result.innings+'回、'+result.out_num+'アウト、'+result.runner+'人が出塁中、'+'ここまで'+result.scores+'点をとられました。1試合平均では'+average_count+'点、とりました');
     $(total_id).text(name_year+'：全'+result.daseki+'打席のうち、1塁打'+result.single+'回、2塁打'+result.double+'回、3塁打'+result.triple+'回、ホームラン'+result.homerun+'回、三振'+result.strikeout+'回、四球'+result.fourball+'回、凡打'+result.mishit+'回、打率は.'+hit_rate+'です。');
     
+    if(result.out_num===3){
+        result.out_num=0;
+        result.runner=0;
+        result.innings++;
+        }
+
     if(out_num<3){
     return{
         out_num,
-        score,
-        runner,
+        runner
     }
     }else{
     out_num=0;
     runner=0;
-    inning++;
     return{
         out_num,
-        score,
-        runner,
-        inning
+        runner
         }
     }
 
@@ -180,7 +214,8 @@
 
 // 落合対応
     // 1.1打席毎計算
-    // clickのあとに直接、関数名を入れても動かなくてエラーが出る。
+    // clickのあとに直接、関数名を入れても動かなくてエラーが出る。文法通り、function()とする。
+    // セレクタ部分はコロン付きで引数にしてみたら、そのままjQueryは動いた
     $('#ochiai').on('click',function(){
         simulation(ochiai_result, ochiai_record_standard, "#status_ochiai", "#total_ochiai", '落合1985');
     });
